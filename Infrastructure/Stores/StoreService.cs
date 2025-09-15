47f898e171d4854777ee20b0cf35d5960835746c
trokhin87 <trokhin87@gmail.com>
2025-09-15 23:46:26 +0300

    Сделал  StoreService.cs

---
+++ Infrastructure/Stores/StoreService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Concurrent;
+using Application;
+using Domain.Models;
+
+namespace Infrastructure.Stores;
+
+public class StoreService:IStoreService
+{
+    private readonly ConcurrentDictionary<Guid,List<ErrorRecord>> _storage=new();
+    public Guid Save(IEnumerable<ErrorRecord> errorRecords)
+    {
+        var id = Guid.NewGuid();
+        _storage[id] = new List<ErrorRecord>(errorRecords);
+        return id;
+    }
+
+    public IEnumerable<ErrorRecord> Get(Guid id)
+    {
+        return _storage[id];
+    }
+
+    public void Remove(Guid id)
+    {
+        _storage.TryRemove(id, out _);
+    }
+}
