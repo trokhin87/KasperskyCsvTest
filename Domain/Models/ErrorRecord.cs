bc155edaa64fafe48e0ab06c67966b046b39f7db
trokhin87 <trokhin87@gmail.com>
2025-09-15 20:22:52 +0300

    Добавил описание Ошибки

---
+++ Domain/Models/ErrorRecord.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Models;
+
+public class ErrorRecord
+{
+    public DateTimeOffset Timestamp { get; init; }
+    public Severity Severity { get; init; }
+    public string Product { get; init; }
+    public string Version { get; init; }
+    public string ErrorCode { get; init; }
+}
