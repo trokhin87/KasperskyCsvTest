f846b02e152062c64936be274db9c814b8c7e0a1
trokhin87 <trokhin87@gmail.com>
2025-09-16 10:46:35 +0300

    Реализовал GenerateTime.cs, который будет использваоться дальше для генерации ошибок

---
+++ Infrastructure/GenerateErrors/GenerateTime.cs
@@ -0,0 +1,21 @@
+﻿using Application;
+
+namespace Infrastructure.GenerateErrors;
+
+public class GenerateTime:IGenerateTime
+{
+    private readonly Random _random = new();
+    private readonly DateOnly _date;
+
+    public GenerateTime(DateOnly date)
+    {
+        _date = date;
+    }
+    public DateTimeOffset Generate(DateTime Day)
+    {
+        int hour = _random.Next(0, 24);
+        int minute = _random.Next(0, 60);
+        int second = _random.Next(0, 60);
+        return _date.ToDateTime(new TimeOnly(hour,minute,second));
+    }
+}
