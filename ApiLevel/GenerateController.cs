2790ced553b2eab33bfdea6950a918c05ab35b40
trokhin87 <trokhin87@gmail.com>
2025-09-16 23:38:13 +0300

    написал контроллер генерации

---
+++ ApiLevel/GenerateController.cs
@@ -0,0 +1,24 @@
+﻿using Application;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiLevel;
+
+[ApiController]
+[Route("api/[controller]")]
+public class GenerateController:ControllerBase
+{
+    private readonly IGeneratorService _generatorService;
+
+    public GenerateController(IGeneratorService generatorService)
+    {
+        _generatorService = generatorService;
+    }
+    
+    [HttpPost("")]
+    public async Task<IActionResult> Generate(int count = 10000)
+    {
+        var id = await _generatorService.GenerateAsync(count);
+        return Ok(new { Id = id, Message = $"{count} records generated" });
+    }
+}
