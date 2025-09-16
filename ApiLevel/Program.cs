d3eb131233b9e275b84a6092b5aed89130ecd516
trokhin87 <trokhin87@gmail.com>
2025-09-16 22:46:51 +0300

    Зарегестрировал сервисы в main

--- ApiLevel/Program.cs
+++ ApiLevel/Program.cs
@@ -1,6 +1,20 @@
+using Application;
+using Infrastructure;
+using Infrastructure.GenerateErrors;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+
+
+builder.Services.AddSingleton<IGenerateTime, GenerateTime>();
+builder.Services.AddSingleton<IGenerateSeverity, GenerateSeverity>();
+builder.Services.AddSingleton<IGenerateProduct, GenerateProduct>();
+builder.Services.AddSingleton<IGenerateVersion, GenerateVersion>();
+builder.Services.AddSingleton<IGenerateErrorCode, GenerateErrorCode>();
+builder.Services.AddSingleton<IErorRecordGenerator, ErrorRecordGenerator>();
+
 
 var app = builder.Build();
 
