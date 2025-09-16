1d0cca41ff1825d669f683224d4d50f64d583e5f
Евгений Трохин <trokhin87@gmail.com>
2025-09-16 17:26:04 +0300

    Сделал реализацию рандома Приоритета

---
+++ Infrastructure/GenerateErrors/GenerateSeverity.cs
@@ -0,0 +1,24 @@
+﻿using Application;
+using Domain.Models;
+
+namespace Infrastructure.GenerateErrors;
+
+public class GenerateSeverity:IGenerateSeverity
+{
+    private readonly WeightedRandom<Severity> _picker;
+    public GenerateSeverity()
+    {
+        var weights = new Dictionary<Severity, int>
+        {
+            {Severity.Critical, 5 },
+            {Severity.High, 25 },
+            {Severity.Normal,40},
+            {Severity.Low,20}
+        };
+        _picker=new WeightedRandom<Severity>(weights);
+    }
+    public Severity Generate()
+    {
+        return _picker.Pick();
+    }
+}
