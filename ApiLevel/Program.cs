761815768247befa05cebaf8cbe33c15ca328a2b
trokhin87 <trokhin87@gmail.com>
2025-09-16 23:37:57 +0300

    Зарегестрировал зависимости, добавил сваггер

--- ApiLevel/Program.cs
+++ ApiLevel/Program.cs
@@ -1,25 +1,36 @@
 using Application;
 using Infrastructure;
 using Infrastructure.GenerateErrors;
+using Infrastructure.Stores;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-
-builder.Services.AddSingleton<IGenerateTime, GenerateTime>();
+var date = DateOnly.FromDateTime(DateTime.Now);
+builder.Services.AddSingleton<IGenerateTime>(sp=>new GenerateTime(date));
 builder.Services.AddSingleton<IGenerateSeverity, GenerateSeverity>();
 builder.Services.AddSingleton<IGenerateProduct, GenerateProduct>();
 builder.Services.AddSingleton<IGenerateVersion, GenerateVersion>();
 builder.Services.AddSingleton<IGenerateErrorCode, GenerateErrorCode>();
 builder.Services.AddSingleton<IErorRecordGenerator, ErrorRecordGenerator>();
 
+builder.Services.AddSingleton<IStoreService, StoreService>();
+builder.Services.AddSingleton<IGeneratorService,GeneratorService >();
+builder.Services.AddSingleton<IAgregationService, AggregationService>();
 
+builder.Services.AddControllers();
 var app = builder.Build();
-
+app.MapControllers();
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+    });
     app.MapOpenApi();
 }
 
