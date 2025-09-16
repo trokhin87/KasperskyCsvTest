3ffc50bbab0c7755db88dae8d5b32a10368fbb5a
Евгений Трохин <trokhin87@gmail.com>
2025-09-16 17:34:11 +0300

    Сделал GenerateProduct.cs используя веса

---
+++ Infrastructure/GenerateErrors/GenerateProduct.cs
@@ -0,0 +1,24 @@
+﻿using Application;
+
+namespace Infrastructure.GenerateErrors;
+
+public class GenerateProduct:IGenerateProduct
+{
+    private readonly WeightedRandom<string> _picker;
+
+    public GenerateProduct()
+    {
+        var weights = new Dictionary<string, int>()
+        {
+            {"A", 15},
+            {"B", 20},
+            {"C", 25},
+            {"D", 40}
+        };
+        _picker=new WeightedRandom<string>(weights);
+    }
+    public string Generate()
+    {
+        return _picker.Pick();
+    }
+}
